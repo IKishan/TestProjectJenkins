032b3da9fe539aa32c3cc58503be30973e3b8a35
IKishan <ishan.ik.kumar@gmail.com>
2020-04-29 12:30:31 +0530

    Correction

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 import java.io.*;
 class HelloWorld{
 	public static void main(String args[]){
-		System.out.printlb("Hello World -- This is Jenkins!");
+		System.out.println("Hello World -- This is Jenkins!");
 	}
 }
